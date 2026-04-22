9cab5aec5ec7e79dd2840039df0ceee56d3a3fe6
ApostleAbe <65122736+ApostleAbe@users.noreply.github.com>
2026-04-22 11:45:08 -0400

    Add files via upload

---
+++ UTCU_Code.ino
@@ -0,0 +1,20 @@
+#define TxEN      D0
+#define DPSK      D1
+#define TO        D5
+#define FRO       D7
+#define ANTSELRD  D9
+#define ANTSEL1   D10
+#define ANTSEL2   D11
+#define ANTSEL3   D12
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
+
